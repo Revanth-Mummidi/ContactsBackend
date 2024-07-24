48425ab9b11461a2c61a259e01b21337058fae4c
Revanth-Mummidi <mummidirevanth@gmail.com>
2024-07-24 16:21:21 +0530

    .

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { errorHandler } = require("./middleware/errorhandler");
 const { connectDb } = require("./config/dbConnection");
 
@@ -10,10 +11,14 @@ connectDb();
 const app=express();
 
 const port=process.env.PORT || 5000;
-
+app.use(cors({
+    origin: true, // Your React development server URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }));
 app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoutes"));
 app.use("/api/users",require("./routes/userRoutes"));
+
 app.use(errorHandler);
 
 app.listen(port,()=>{
